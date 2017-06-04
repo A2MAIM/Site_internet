a6056b850bd85d7068e3a24953ddafa1bfea03f7
A2MAIM <a2maim.paris6@gmail.com>
2017-06-04 17:53:16 +0200

    Mise à jour de la derniere modification

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-﻿<div id="footer">
-	<p>Dernière mise à jour : 21 fevrier 2017 <br>
+<div id="footer">
+	<p>Dernière mise à jour : 04 juin 2017 <br>
 	Copyright &copy; 2011 A2MAIM Template design by <a href="http://andreasviklund.com/">Andreas Viklund</a></p>
 </div>
